0932b22031cd1312cd2cf9e5f3278b4974751877
Juanesillo <j.trivinon@uniandes.edu.co>
2024-10-27 22:53:01 -0500

    parte 2

--- Script/main.js
+++ Script/main.js
@@ -10,17 +10,47 @@ totalCreditElm.innerHTML = "" + getTotalSeassons(dataSeries);
 
 function renderCoursesInTable(series) {
     series.forEach(function (serie) {
-        console.log(serie.imagen);
         var trElement = document.createElement("tr");
-        trElement.innerHTML = `
-            <td>${serie.id}</td>
-            <td>${serie.nombre}</td>
-            <td>${serie.canal}</td>
-            <td>${serie.temporada}</td>
-            <td><img src="${serie.imagen}"  style="width: 50px; height: 50px;" /></td>`;
+
+        // Cambia el contenido del nombre a un enlace que maneje el clic
+        var nombreCelda = "<td><a href='#' class='serie-link' data-id='" + serie.id + "'>" + serie.nombre + "</a></td>";
+
+        trElement.innerHTML = "<td>" + serie.id + "</td>" +
+            nombreCelda +
+            "<td>" + serie.canal + "</td>" +
+            "<td>" + serie.temporada + "</td>" +
+            "<td><img src='" + serie.imagen + "' alt='" + serie.nombre + "' /></td>";
+
         seriesTbody.appendChild(trElement);
     });
+
+    // Agrega el evento click a los enlaces de las series
+    const serieLinks = document.querySelectorAll('.serie-link');
+    serieLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault(); // Prevenir el comportamiento predeterminado del enlace
+            const serieId = this.getAttribute('data-id');
+            mostrarDetallesSerie(serieId);
+        });
+    });
 }
+
+// Nueva función para mostrar los detalles de la serie
+function mostrarDetallesSerie(id) {
+    const serie = dataSeries.find(serie => serie.id === parseInt(id));
+    if (serie) {
+        document.getElementById('detalle-nombre').innerText = serie.nombre;
+        document.getElementById('detalle-imagen').src = serie.imagen;
+        document.getElementById('detalle-canal').innerText = "Canal: " + serie.canal;
+        document.getElementById('detalle-temporada').innerText = "Temporadas: " + serie.temporada;
+        document.getElementById('detalle-resumen').innerText = serie.resumen;
+        document.getElementById('detalle-link').href = serie.link;
+
+        // Mostrar la card de detalles
+        document.getElementById('serie-details').style.display = 'block';
+    }
+}
+
 
 
 function getTotalSeassons(series) {
